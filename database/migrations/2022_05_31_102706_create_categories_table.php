a9a4ae2e5b1986d542473655dbc0ea45bc6cc7e2
viaigor7 <viaigor7@gmail.com>
2022-06-01 16:08:03 +0300

    add category

---
+++ database/migrations/2022_05_31_102706_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+
+        Schema::table('cars', function (Blueprint $table) {
+            $table->foreign('category_id', 'car_category_fk')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
