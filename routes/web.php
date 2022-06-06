309a752780745eef8038b19fa7e197c5e75548b3
viaigor7 <viaigor7@gmail.com>
2022-06-06 08:38:54 +0300

    add new Routers

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
+Route::post('/home', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
 Route::post('/home/categiry', [App\Http\Controllers\HomeController::class, 'createCategiry'])->name('categiry');
 Route::post('/home/driver', [App\Http\Controllers\HomeController::class, 'createDriver'])->name('driver');
+Route::get('/home/{car}/edit', 'CarController@edit')->name('car.edit');
+Route::patch('/home/{car}', 'CarController@update')->name('car.update');
+Route::delete('/home/{car}', 'CarController@destroy')->name('car.delete');
+Route::get('/home/category/{category}/edit', 'CarController@categoryEdit')->name('category.edit');
+Route::patch('/home/category/{category}', 'CarController@categoryUpdate')->name('category.update');
+Route::delete('/home/category/{category}', 'CarController@categoryDestroy')->name('category.delete');
+Route::get('/home/driver/{driver}/edit', 'CarController@driverEdit')->name('driver.edit');
+Route::patch('/home/driver/{driver}', 'CarController@driverUpdate')->name('driver.update');
+Route::delete('/home/driver/{driver}', 'CarController@driverDestroy')->name('driver.delete');
